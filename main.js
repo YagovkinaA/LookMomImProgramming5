15ecf5398a5704be8b180874f002255a2fe978a6
YagovkinaA <90439169+YagovkinaA@users.noreply.github.com>
2021-10-11 23:47:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", function (event){});
 
-function calculat() {
+function calc() {
     let number = Number(document.getElementById("Warpstones_Count").value);
     let price = Number(document.getElementById("Warpstone_Price").value);
 
